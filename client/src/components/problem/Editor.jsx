6eb480b213efbac7e98ce4fcfb80777f1ed76211
aryanpatel3 <patel3aryan@gmail.com>
2023-03-18 18:01:47 -0500

    sync code editor colour mode with app

--- client/src/components/problem/Editor.jsx
+++ client/src/components/problem/Editor.jsx
@@ -1,9 +1,10 @@
 import CodeMirror from "@uiw/react-codemirror";
 import { python } from "@codemirror/lang-python";
 import { EditorView } from "@codemirror/view";
+import { useColorMode } from "@chakra-ui/react";
 
-export function Editor(props) {
-  const { code, onCodeChange } = props;
+export function Editor({ code, onCodeChange }) {
+  const { colorMode } = useColorMode();
 
   const fixedHeightEditor = EditorView.theme({
     "&": { height: "500px" },
@@ -16,7 +17,7 @@ export function Editor(props) {
       value={code}
       extensions={[python(), fixedHeightEditor]}
       onChange={onCodeChange}
-      theme={"dark"}
+      theme={colorMode}
     />
   );
 }
